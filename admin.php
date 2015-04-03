bbea898455bf42496470357537159465dffbed17
Test <test>
2015-04-03 16:09:44 -0400

    Admin users table added

--- admin.php
+++ admin.php
@@ -7,6 +7,11 @@
     <meta charset="utf-8" />
 	<title>EnterpriseHardware.com - Sitemap</title>
     <link rel="stylesheet" type="text/css" href="extra.css" />
+    <style>
+        .dataChanged {
+            border:  1px solid red;
+        }
+    </style>
 </head>
 
 <body>
@@ -27,13 +32,13 @@
                     <li><a href="login.php">Login</a></li>
                     <li><a href="catalog.html">Catalog</a></li>
                     <li><a href="contact.html">Contact</a></li>
-                    <li class="active"><a href="sitemap.html">Sitemap</a></li>
+                    <li><a href="sitemap.html">Sitemap</a></li>
                     <li><a href="review.html">Review</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="userupdate.php">Profile Update</a></li>
                     <li><form action="logout.php" method="post">
-                             <input type="submit" value="Logout" >
+                             <input type="submit" value="Logout" ></form>
                     </li>
 					<li><a href="#">Help</a></li>
                 </ul>
@@ -47,49 +52,73 @@
         <h1>Admin: Edit Users</h1>
 
         <form name="admin">
-        <table>
+        <table class="table table-striped">
             
-            <thead>
-            <tr><th>Delete</th>
-            <th>Username</th>
-            <th>Password</th>
+            <thead><tr>
+                <th>Delete</th>
+                <th>Username</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            <th>Phone #</th>
+            <th>Street #</th>
+            <th>Street Name</th>
+            <th>Unit #</th>
+            <th>City</th>
+            <th>Province</th>
+            <th>Postal Code</th>
+
+                <th>Password</th>
             </tr></thead>
             
-            <tr>
-                <td><input type="checkbox" name="delete"></td>
-                <td contenteditable>Musharraf </td>
-                <td contenteditable>password </td></tr>
+          <tbody>
 
-            <tr>
+            <?php
 
-                <td><input type="checkbox" name="delete"></td>
-                <td contenteditable>Musharraf2 </td>
-                <td contenteditable>password2 </td>
+                require_once ("Includes/var_init.php"); 
+                require_once  ("Includes/connectDB.php");
+                require_once  ("Includes/session.php");
+                                    //echo "Hello";
+                
+                    $query = "SELECT * FROM users";
+                    $result = $databaseConnection->query($query);
+                    if ($result->num_rows > 0) { 
+                        while($row = $result->fetch_assoc()){
+                            echo "<tr>";
+                            echo '<td><input type="checkbox" name="delete"></td>';
+				            echo "<td contenteditable>" . $row["username"] . "</td>";
+                            echo "<td contenteditable>" . $row["fname"] . "</td>";
+                            echo "<td contenteditable>" . $row["lname"] . "</td>";
+                            echo "<td contenteditable>" . $row["email"] . "</td>";
+                            echo "<td contenteditable>" . $row["phone_num"] . "</td>";
+                            echo "<td contenteditable>" . $row["street_num"] . "</td>";
+                            echo "<td contenteditable>" . $row["street_name"] . "</td>";
+                            echo "<td contenteditable>" . $row["unit_num"] . "</td>";
+                            echo "<td contenteditable>" . $row["city"] . "</td>";
+                            echo "<td contenteditable>" . $row["province"] . "</td>";
+                            echo "<td contenteditable>" . $row["postalcode"] . "</td>";
+                            echo "<td contenteditable>" . $row["password"] . "</td>";
+				            echo "</tr>";
+			            }
+                    } else {
+                        echo "no resulsts";
+                    }
 
-                <? 
 
-                        //$statement = $databaseConnection->prepare("SELECT * from users");
-                        //$statement->execute();
-
-                        //if($statement->error)
-                        //{
-                        //    die("Database query failed: " . $statement->error);
-                        //}
-
-                        //$statement->bind_result($username, $password);
-                        //while($statement->fetch())
-                        //{
-                        //    //
-                        //}
-
-                        ?>
- 
-            </tr>
-
-
-        </table>
+                ?>
+        </tbody></table>
         </form>
+                        
     </div>
+    <script>
+        var tds = document.querySelectorAll("form td");
+        for (var i = 0; i < tds.length; i++) {
+            tds[i].addEventListener("DOMSubtreeModified", function (ev) {
+                //tds[i].style = "border: 10px solid red;";
+                //tds[i].parentElement.className = "dataChanged";
+            });
+        }
+    </script>
 </body>
 
 	
